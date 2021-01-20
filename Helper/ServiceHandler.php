3224627ac27f2f1e4a9b17a8b45d6494e08485b9
JoseChan <524233828@qq.com>
2021-01-20 22:15:38 +0800

    namespace fix

--- Helper/ServiceHandler.php
+++ Helper/ServiceHandler.php
@@ -17,14 +17,17 @@ class ServiceHandler
     public static function register()
     {
         //读取服务域名及监听的端口
-        $app_name = "helloWorld";
-        $server_host = "127.0.0.1:5200";
+        $app_name = env("APP_NAME", "helloWorld");
+        $server_host = env("LARAVELS_LISTEN_IP", "127.0.0.1") . ":" . env("LARAVELS_LISTEN_PORT". 5200);
         if (empty($app_name)) {
             return false;
         }
 
+        global $client;
+
         //链接zookeeper
         $client = new \Zookeeper("localhost:2181");
+        //将客户端设置为全局变量，不会销毁关闭
 
         $acls = [
             [
@@ -35,6 +38,7 @@ class ServiceHandler
         ];
 
         $client->create("/{$app_name}/{$server_host}", "1", $acls, \Zookeeper::EPHEMERAL);
+
     }
 
     //服务发现
