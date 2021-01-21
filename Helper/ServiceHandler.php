b5c2199e5502230c57d3b134960882d96de03419
JoseChan <524233828@qq.com>
2021-01-21 15:49:59 +0800

    处理启动毁掉

--- Helper/ServiceHandler.php
+++ Helper/ServiceHandler.php
@@ -14,8 +14,8 @@ class ServiceHandler
     public static function register()
     {
         //读取服务域名及监听的端口
-        $app_name = env("APP_NAME", "helloWorld");
-        $server_host = env("LARAVELS_LISTEN_IP", "127.0.0.1") . ":" . env("LARAVELS_LISTEN_PORT", 5200);
+        $app_name = self::getConfigByKey("APP_NAME", "helloWorld");
+        $server_host = self::getConfigByKey("LARAVELS_LISTEN_IP", "127.0.0.1") . ":" . self::getConfigByKey("LARAVELS_LISTEN_PORT", 5200);
         if (empty($app_name)) {
             return false;
         }
@@ -42,6 +42,29 @@ class ServiceHandler
     public static function discover()
     {
 
+    }
+
+    public static function getConfig()
+    {
+        return unserialize((string)file_get_contents($this->getConfPath()));
+    }
+
+    protected static function getConfPath()
+    {
+        return realpath(__DIR__ . '/../') . '/storage/laravels.conf';
+    }
+
+    public static function getConfigByKey($key, $default)
+    {
+        $config = self::getConfig();
+
+        $config = $config['laravel'];
+
+        if (isset($config[$key])) {
+            return $config[$key];
+        }
+
+        return $default;
     }
 
 }
